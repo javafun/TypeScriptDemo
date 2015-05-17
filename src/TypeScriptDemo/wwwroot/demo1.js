aa88ad03dce3c208616bf240d5cd935fca7126de
Vincent Yang <mscsharp@gmail.com>
2015-05-17 12:01:40 +1000

    Initial commit

---
+++ src/TypeScriptDemo/wwwroot/demo1.js
@@ -0,0 +1,6 @@
+// Primitive type annotation
+var name = "steve";
+var heightInCetimeters = 1234.56;
+var isActive = true;
+// Array type annotation
+var names = ["james", "vincent", "Cecila"];
---
+++ src/TypeScriptDemo/wwwroot/demo1.ts
@@ -0,0 +1,8 @@
+﻿// Primitive type annotation
+var name: string = "steve";
+var heightInCetimeters: number = 1234.56;
+var isActive: boolean = true;
+
+// Array type annotation
+
+var names: string[] = ["james", "vincent", "Cecila"];
